9c7c56acfe74fb1f4afa3fe2a94658b5c4b870eb
myvinnie <770601773@qq.com>
2016-04-18 23:45:18 +0800

    Annotations测试

--- app/src/main/java/com/linw/helloaannotations/MainActivity.java
+++ app/src/main/java/com/linw/helloaannotations/MainActivity.java
@@ -2,8 +2,10 @@ package com.linw.helloaannotations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 @EActivity
@@ -12,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.tv_main)
     TextView tv_main;
 
+    @ViewById(R.id.btn_main)
+    Button btn_main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Click(R.id.btn_main)
+    public void settext() {
+        tv_main.setText("hello Annotition");
+    }
+
 }
