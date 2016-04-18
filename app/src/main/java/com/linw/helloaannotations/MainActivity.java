42e55152bcb380163445909557ea35a2396e0a82
myvinnie <770601773@qq.com>
2016-04-18 23:40:30 +0800

    hello Android Annotations

--- app/src/main/java/com/linw/helloaannotations/MainActivity.java
+++ app/src/main/java/com/linw/helloaannotations/MainActivity.java
@@ -2,8 +2,15 @@ package com.linw.helloaannotations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+@EActivity
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.tv_main)
+    TextView tv_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
